aa54c559268b7a1154acec3f5d4fcfae7d30246e
Yuriy <yuriy-k@ukr.net>
2023-01-17 13:55:30 +0300

    some fix

---
+++ src/LinksData.js
@@ -0,0 +1,18 @@
+export const links = {
+  home: {
+    label: 'Home',
+    href: '/',
+  },
+  about: {
+    label: 'About us',
+    href: '/#about',
+  },
+  contact: {
+    label: 'Contact us',
+    href: '/#contact',
+  },
+  help: {
+    label: 'Help page',
+    href: '/#help',
+  },
+};
